33c6f68e9132fab9b4cc316c2f3e0ab30967f303
Caroline Longnecker <cvrieous@gmail.com>
2020-01-08 06:56:28 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,9 +1,10 @@
 class Cat
-  attr_reader :name
+  attr_reader :name, :mood
   attr_accessor :owner
   
   def initialize(name, owner)
     @name = name
     @owner = owner
+    @mood = nervous
   end
 end
